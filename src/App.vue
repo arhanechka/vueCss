ab479182280a75c9d9b6f20440c5ff1faca78904
Arhanechka <aranvic1976@gmail.com>
2018-12-18 21:49:21 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-bind:class="theme" id="app">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+let bg = require('./assets/profile.js');
+export default {
+  name: 'app',
+  
+  data() {
+    return{
+      message: 'Hello, world!',
+      theme: 'simple'
+    }
+  },
+  created(){
+   
+    console.log(bg.color)
+    this.theme = bg.color
+  }
+}
+</script>
+<style lang="scss">
+ @import './assets/css/main';
+ body {
+   background: $background-body
+ }
+ .tasty {
+   background: $background-color2
+  
+ }
+  .simple {
+   background: $background-color1
+    }
+  .sky {
+   background: $background-color
+ }
+  h1 {
+    color: $background-color1;
+  }
+</style>
+
+
